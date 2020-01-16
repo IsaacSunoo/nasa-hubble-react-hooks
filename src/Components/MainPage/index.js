3a6a194337e236eede98f47a5c61075674438c18
Hack Reactor Student <student@hackreactor.com>
2020-01-16 10:45:18 -0700

    added background image to header

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -13,13 +13,14 @@ const MainPage = () => {
     dispatch(getDailyNews());
   }, []);
 
-  const { hdurl, explanation, title, url} = daily;
+  const { hdurl, explanation, title, url, date} = daily;
 
   return (
     <div className='nasa-daily'>
       <ParticlesContainer />
       <h2>{title}</h2>
       <a href={url} ><img src={hdurl} alt='nasa daily img' /></a>
+      <p>{date}</p>
       <p>{explanation}</p>
     </div>
   );
